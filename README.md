6c75f96c1b026a93420acfb3af655cdf72148658
Javi R <4920956+rameerez@users.noreply.github.com>
2025-02-18 13:07:30 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# You're probably looking for the `telegrama` gem
+
+Head to [`telegrama`](https://github.com/rameerez/telegrama)
---
+++ lib/telegrams.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "telegrams/version"
+
+module Telegrams
+  class Error < StandardError; end
+  # Your code goes here...
+end
